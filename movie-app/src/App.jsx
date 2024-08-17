cc96eecf3869ce8aa5d58295455610c52247c260
Ravi Bagdi <ravibagdi799@outlook.com>
2024-08-17 15:54:13 +0530

    Initialize React App

---
+++ movie-app/src/App.jsx
@@ -0,0 +1,17 @@
+import  React from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     Project goes here.
+      
+    </>
+  )
+}
+
+export default App
+
